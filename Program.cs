02a02c81aaa5918b947f573c6ba1e95b4876d39a
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-05 23:04:19 +0700

    a

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap so: ");
+        int so = Convert.ToInt32(Console.ReadLine());
+        if(so < 2){
+            Console.WriteLine(so + " khong phai la so nguyen to");
+        }else {
+            int i = 2;
+            bool check = true;
+            while (i <= Math.Sqrt(so))
+            {
+                if (so % i == 0)
+                {
+                    check = false;
+                    break;
+                }
+                i++;
+            }if(check){
+                Console.WriteLine(so + " la so nguyen to");                
+            }else {
+                Console.WriteLine(so + " khong phai la so nguyen to");
+            }
+        }        
+    }
+}
